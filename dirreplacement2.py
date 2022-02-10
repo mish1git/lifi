8b379721741eacc300cee969ff27174ddb70be55
mish <38535438+mish1git@users.noreply.github.com>
2022-02-10 18:34:16 +0000

    Add files via upload

--- dirreplacement2.py
+++ dirreplacement2.py
@@ -3,7 +3,21 @@ import getopt
 import os
 from pathlib import *
 p=os.getcwd()
-dirs = os.listdir(p)
+dirs = sorted(os.listdir(p))
+
+sys.argv = sys.argv[1:]
+names = []
+while sys.argv and sys.argv[0] == '-d':
+    dirs = sorted(os.listdir(sys.argv[1]))
+
+    for file in dirs:
+        print(file)
+
+    if dirs == []:
+        print("This directory is empty")
+
+    exit()
+
 
 #p is directory path
 for file in dirs:
